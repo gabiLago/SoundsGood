bae8bb2e7db606c5f7be2a086ae78a2aa9182bda
Gabi Lago Blasco <gabi@MacBook-Pro-de-Gabi.local>
2019-11-11 20:21:16 +0100

    Dismiss button improved

--- sounds_good/lib/screens/views/edit_profile_view.dart
+++ sounds_good/lib/screens/views/edit_profile_view.dart
@@ -54,18 +54,23 @@ class _EditProfileViewState extends State<EditProfileView> {
                       Positioned(
                         top: 0,
                         right: 0,
-                        child: RawMaterialButton(
-                          padding: EdgeInsets.all(0),
-                          fillColor: Colors.white,
+                        child: Container(
+                          width: 50.0,
+                          height: 50.0,
+                          child: FloatingActionButton(
+                          
+                          backgroundColor: Colors.white,
                           elevation: 4,
                           shape: CircleBorder(),
                           child: Icon(
+                            
                             Icons.close,
                             color: Colors.black,
                             semanticLabel: 'Close Screen',
                           ),
                           onPressed: () {},
                         ),
+                      ),
                       ),
                       EditProfileHeader(
                         nameController: this.nameController,
